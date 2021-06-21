59d4ae16b7e5fad1532498773985a3d70456c587
masafumi <masa455.wv@gmail.com>
2021-06-22 02:09:58 +0900

    微調整

--- docs/login.php
+++ docs/login.php
@@ -25,14 +25,14 @@
                     <h1>~~~~~にログイン</h1>
                 </div>
                 <div class="input_styles">
-                    <input type="text" onfocus="animation1()" onblur="animationout1()" id="input_name">
+                    <input type="text" onfocus="animation1()" onblur="animationout1()" id="input_name1">
                     <div id="anime1" class="placeholder1">名前を入力してください</div>
                 </div>
 
                 <div class="space_24"></div>
 
                 <div class="input_styles">
-                    <input type="text" onfocus="animation2()" onblur="animationout2()">
+                    <input type="text" onfocus="animation2()" onblur="animationout2()" id="input_name2">
                     <div id="anime2" class="placeholder2">パスワードを入力してください</div>
                 </div>
 
@@ -59,16 +59,15 @@
         }
 
         function animationout1() {
-           
-            if(inputChange(event) == "") {
+
+            if (inputChange(event) == "") {
                 placeholder1.classList.add('placeholder1');
                 placeholder1.classList.remove('done');
                 placeholder1.classList.remove('placeholder_animation1');
-         
+
             } else {
                 placeholder1.classList.add('done');
                 placeholder1.classList.remove('placeholder1');
-                console.log('hell')
             }
         }
 
@@ -77,24 +76,29 @@
         }
 
         function animationout2() {
-            if(inputChange(event) == "") {
+            if (inputChange(event) == "") {
                 placeholder2.classList.add('placeholder2');
                 placeholder2.classList.remove('done');
                 placeholder2.classList.remove('placeholder_animation2');
-         
+
             } else {
                 placeholder2.classList.add('done');
                 placeholder2.classList.remove('placeholder2');
-                console.log('hell')
             }
         }
 
+
+        // ---input textの入力された値をゲット---
         function inputChange(event) {
+            console.log(event.currentTarget.value);
             return event.currentTarget.value;
         }
 
-        const input_name = document.getElementById('input_name');
-        input_name.addEventListener('input',inputChange);
+        const input_name1 = document.getElementById('input_name1');
+        const input_name2 = document.getElementById('input_name2');
+        input_name1.addEventListener('input', inputChange);
+        input_name2.addEventListener('input', inputChange);
+        // ________
     </script>
 
 
