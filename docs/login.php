7f4e55afbafdcf7c94bca87bed235874d051886e
masafumi <masa455.wv@gmail.com>
2021-06-21 16:17:42 +0900

    logincss追加　写真追加

--- docs/login.php
+++ docs/login.php
@@ -5,28 +5,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ログイン画面</title>
-    <link href="css/style.css" rel="stylesheet">
+    <link href="css/login.css" rel="stylesheet">
 </head>
 
 <body>
     <header>
-        <h3>The Beatles</h3>
+        <div class="img_rogo">
+            <img src="beatles_logo06.png" alt="aaaaaa">
+        </div>
     </header>
     <main>
         <!-- action と method 忘れずに！ -->
+        
         <form action="login_act.php" method="post" autocomplete="off">
-            <h2>ログイン</h2>
-            <div>
-                <!-- name属性わすれずに！dbと同じにする -->
-                ユーザーネーム <input type="text" name="username" placeholder="name">
+
+            <div class="form_container">
+                <h2>ログイン</h2>
+                <div>
+                    <!-- name属性わすれずに！dbと同じにする -->
+                    ユーザーネーム <input type="text" name="username" placeholder="name">
+                </div>
+                <div>
+                    パスワード <input type="password" name="password" placeholder="password">
+                </div>
+                <div>
+                    <button class="gradient1">ログイン</button>
+                </div>
+                <a href="signup.php" class="btn">新規登録はこちらから</a>
             </div>
-            <div>
-                パスワード <input type="password" name="password" placeholder="password">
-            </div>
-            <div>
-                <button class="gradient1">ログイン</button>
-            </div>
-            <a href="signup.php" class="btn">新規登録はこちらから</a>
+
         </form>
     </main>
 
