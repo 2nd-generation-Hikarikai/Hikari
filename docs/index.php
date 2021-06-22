ac42aed1bda6cdb76ded070f66170139fc358175
hikaritsumagari <hikari.tsumagari@gmail.com>
2021-06-22 15:15:30 +0900

    indexできました

--- docs/index.php
+++ docs/index.php
@@ -7,14 +7,72 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Beatles</title>
     <link href="css/style.css" rel="stylesheet">
+    <link href="css/welcome.css" rel="stylesheet">
 </head>
 
 <body>
     <header>
-        <img src="./img/beatles_rogo06.jpg" alt="aaaaaa">
-        <h2>The Beatles</h2>
+        <img src="./img/beatles_logo05.png" alt="aaaaaa">
         <a href="login.php" class="gradient1">ログイン</a>
     </header>
+    <main>
+        <ul class="img_wrap">
+            <li><img src="img/beatles.jpg" alt=""></li>
+            <li><img src="img/beatles09.jpg" alt=""></li>
+            <li><img src="img/beatles13.jpg" alt=""></li>
+            <li><img src="album_img/AbbeyRoad.jpg" alt=""></li>
+            <li><img src="img/beatles10.jpg" alt=""></li>
+            <li><img src="img/beatles_04.jpg" alt=""></li>
+            <li><img src="img/beatles08.jpg" alt=""></li>
+            <li><img src="img/beatles11.jpg" alt=""></li>
+            <li><img src="img/beatles06.jpeg" alt=""></li>
+            <li><img src="img/beatles.jpg" alt=""></li>
+            <li><img src="img/beatles_logo04.jpg" alt=""></li>
+            <li><img src="img/beatles14.jpg" alt=""></li>
+            <li><img src="album_img/Help.jpg" alt=""></li>
+            <li><img src="img/beatles09.jpg" alt=""></li>
+            <li><img src="img/beatles02.jpg" alt=""></li>
+            <li><img src="album_img/AHardDaysNight.jpg" alt=""></li>
+            <li><img src="img/beatles13.jpg" alt=""></li>
+            <li><img src="album_img/YellowSubmarine.jpg" alt=""></li>
+            <li><img src="img/beatles08.jpg" alt=""></li>
+            <li><img src="img/beatles03.jpg" alt=""></li>
+            <li><img src="img/beatles07.jpg" alt=""></li>
+            <li><img src="album_img/AbbeyRoad.jpg" alt=""></li>
+            <li><img src="img/beatles15.jpg" alt=""></li>
+            <li><img src="img/beatles.jpg" alt=""></li>
+            <li><img src="album_img/AbbeyRoad.jpg" alt=""></li>
+            <li><img src="img/beatles10.jpg" alt=""></li>
+            <li><img src="img/beatles_04.jpg" alt=""></li>
+            <li><img src="img/beatles08.jpg" alt=""></li>
+            <li><img src="img/beatles11.jpg" alt=""></li>
+            <li><img src="img/beatles06.jpeg" alt=""></li>
+            <li><img src="img/beatles.jpg" alt=""></li>
+            <li><img src="img/beatles_logo04.jpg" alt=""></li>
+        </ul>
+
+        <div class="main_wrap transparent">
+            <h1 class="transparent">Beatlesの曲が聴き放題</h1>
+
+            <h2 class="transparent">ああああああ</h2>
+
+            <form action="signup.php" method="post" autocomplete="off" class="transparent">
+
+                <div class="transparent">
+                    <p class="transparent">メールアドレスを入力してサインアップ開始！</p>
+                </div>
+                <div class="transparent">
+                    <input type="email" placeholder="email">
+                </div>
+                <div class="space_24 transparent"></div>
+                <button class="gradient1">はじめる</button>
+
+        </div>
+
+        </form>
+        </div>
+
+    </main>
 </body>
 
 </html>
