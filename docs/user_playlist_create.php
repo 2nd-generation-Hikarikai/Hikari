668985e9e2a50018a4a0376c1f43da4073a5641c
hikaritsumagari <hikari.tsumagari@gmail.com>
2021-06-23 14:31:08 +0900

    playlistむずい

--- docs/user_playlist_create.php
+++ docs/user_playlist_create.php
@@ -21,9 +21,10 @@ $playlist_name = $_POST['playlist_name'];
 
 $pdo = connect_to_db();
 
-$sql = 'INSERT INTO playlists_table(playlist_id, user_id, playlist_name, is_deleted,created_at, updated_at) VALUES(NULL, :user_id, :playlist_name, sysdate(), sysdate())';
-
+$sql = 'INSERT INTO playlists_table(playlist_id, user_id, playlist_name, created_at, updated_at) VALUES(NULL, :user_id, :playlist_name, sysdate(), sysdate())';
+// exit('ok');
 $stmt = $pdo->prepare($sql);
+$stmt->bindValue(':user_id', $_SESSION["user_id"], PDO::PARAM_INT);
 $stmt->bindValue(':playlist_name', $playlist_name, PDO::PARAM_STR);
 
 $status = $stmt->execute();
