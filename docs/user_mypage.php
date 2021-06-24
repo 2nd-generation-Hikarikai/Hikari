5f5489b34ab8e40798e2a01bddd3d1e6601960da
masafumi <masa455.wv@gmail.com>
2021-06-24 14:57:51 +0900

    プレイリストに入った！

--- docs/user_mypage.php
+++ docs/user_mypage.php
@@ -14,10 +14,10 @@ $pdo = connect_to_db();
 // プレイリスト名・曲名・曲・画像を表示する
 // SELECT文（DB結合）
 $sql = 'SELECT * FROM playlist_create_table INNER JOIN playlists_table ON playlists_table.playlist_id = playlist_create_table.playlist_id
-INNER JOIN music_table ON playlist_create_table.music_id = music_table.music_id';
+INNER JOIN music_table ON playlist_create_table.music_id = music_table.music_id WHERE user_id=?';
 
 $stmt = $pdo->prepare($sql);
-$status = $stmt->execute(); // SQLを実行 $statusに実行結果(取得したデータではない！)
+$status = $stmt->execute([$_SESSION['user_id']]); // SQLを実行 $statusに実行結果(取得したデータではない！)
 // var_dump($status);
 // exit;
 
@@ -89,7 +89,7 @@ unset($record);
 
 <body>
     <header>
-        <a href="beatles.php" class="btn"><img src="img/beatles_logo05.png" alt=""></a>
+        <!-- <a href="beatles.php" class="btn"><img src="img/beatles_logo05.png" alt=""></a> -->
         <h3>My Playlist</h3>
         <a href="logout.php" class="btn">logout</a>
         <a href="user_edit.php" class="btn">プロフィール編集</a>
